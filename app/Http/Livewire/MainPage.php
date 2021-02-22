240a29e45f16eb5c5f08999bf36f632192167565
rednum <rednum89@gmail.com>
2021-02-22 15:29:00 +0100

    basic resizing

--- app/Http/Livewire/MainPage.php
+++ app/Http/Livewire/MainPage.php
@@ -2,12 +2,64 @@
 
 namespace App\Http\Livewire;
 
+use Dotenv\Exception\ValidationException;
+use Exception;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Intervention\Image\Facades\Image;
 
 class MainPage extends Component
 {
+    use WithFileUploads;
+
+    public $imageFile = null;
+    public $imageFileEdit;
+
+    public $width;
+    public $height;
+    public $filters = [
+        'width' => 0,
+        'height' => 0,
+        'aspect_ratio' => true
+    ];
+
+    protected $rules = [
+        'filters.width' => 'required|min:1|numeric',
+        'filters.height' => 'required|min:1|numeric',
+    ];
+
     public function render()
     {
         return view('livewire.main-page');
+    }
+
+    public function updatedImageFile($value)
+    {
+
+        $extension = pathinfo($value->getFilename(), PATHINFO_EXTENSION);
+        if (!in_array($extension, ['png', 'jpg', 'jpeg', 'bmp', 'gif'])) {
+            $this->reset();
+        }
+
+        $this->validate([
+            'imageFile' => 'mimes:png,jpg,jpeg,bmp,gif|max:1024',
+        ]);
+        $tempImage = Image::make($this->imageFile);
+        $this->imageFileEdit = $tempImage->encode('data-url')->getEncoded();
+        $this->filters['width'] = $tempImage->getWidth();
+        $this->filters['height'] = $tempImage->getHeight();
+    }
+
+    public function updatedFilters()
+    {
+        $this->validate();
+
+        $this->imageFileEdit = Image::make($this->imageFile)
+        ->resize($this->filters['width'], $this->filters['height'], function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        })
+        ->encode('data-url')
+        ->getEncoded();
     }
 }
--- resources/views/livewire/main-page.blade.php
+++ resources/views/livewire/main-page.blade.php
@@ -1,3 +1,55 @@
-<x-layouts.app>
-    Main page
-</x-layouts.app>
+    <div class="w-full flex min-h-screen">
+        <div class="bg-blue-50 p-4 w-full justify-center">
+            <div class="flex flex-col items-center justify-center mb-10">
+                <h1 class="text-lg text-center mb-2">Select image to edit</h1>
+                <input type="file" wire:model="imageFile" name="imageFile2">
+            </div>
+            <div class="flex">
+
+                {{-- ORIGINAL PREVIEW --}}
+                <div class="p-4 flex-1 flex flex-col @if($imageFile)border border-gray-400 rounded @endif">
+                    @if ($imageFile)
+                    Original Image Preview:
+                    <div class="flex flex-col justify-center items-center h-full">
+                        <img src="{{ $imageFile->temporaryUrl() }}">
+
+                    </div>
+                    @endif
+                </div>
+                @error('imageFile')
+                    <div class="text-xs uppercase text-red-500 py-1">{{ $message }}</div>
+                @enderror
+
+                {{-- FILTERS --}}
+                <div class="p-4 flex flex-col items-end">
+                    @if ($imageFile)
+                    <label>Width: 
+                        <input type="number" wire:model.lazy="filters.width" class="p-2 rounded">
+                    </label>
+                    @error('filters.width')
+                        <div class="text-xs uppercase text-red-500 py-1">{{ $message }}</div>
+                    @enderror
+                    <label>Height: 
+                        <input type="number" wire:model.lazy="filters.height" class="p-2 rounded">
+                    </label>
+                    @error('filters.height')
+                        <div class="text-xs uppercase text-red-500 py-1">{{ $message }}</div>
+                    @enderror
+                    @endif
+                </div>
+                
+
+                {{-- EDIT PREVIEW --}}
+                <div class="p-4 flex-1 flex flex-col @if($imageFile)border border-gray-400 rounded @endif">
+                    @if ($imageFileEdit)
+                    Edit Preview:
+                    <div class="flex flex-col justify-center items-center h-full">
+                        <img src="{{ $imageFileEdit }}">
+                    </div>
+                    @endif
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
